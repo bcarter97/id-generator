d7da68642e59afabc49f28347542764cb334e663
Ben Carter <benjohn.carter@gmail.com>
2021-12-10 17:48:46 +0000

    build: remove scalatest it dependency (#6)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.10" % "it,test"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.10" % Test
 
   object Cats {
     private val cats = "2.7.0"
