9218448c98b4e52949431f0bfeb0cb28c06f3fda
Ben Carter <benjohn.carter@gmail.com>
2021-12-12 17:54:46 +0000

    refactor: make API easier to understand, add sample

--- src/test/scala/io/github/bcarter97/idgenerator/GeneratorSpec.scala
+++ src/test/scala/io/github/bcarter97/idgenerator/GeneratorSpec.scala
@@ -10,16 +10,16 @@ class GeneratorSpec extends AnyWordSpecLike with Matchers with ScalaFutures with
   "Generator" should {
     "generate a reproducable UUID" in {
       val generator = Generator(maxIndex = 100)
-      val id        = generator.indexToId(50)
-      val id2       = generator.indexToId(50)
+      val id        = generator.id(50)
+      val id2       = generator.id(50)
 
       id2 shouldBe id
     }
 
     "generate a reproducable amount of subIds given an id" in {
       val generator  = Generator(maxIndex = 100)
-      val id         = generator.indexToId(50)
-      val id2        = generator.indexToId(50)
+      val id         = generator.id(50)
+      val id2        = generator.id(50)
       val numSubIds  = generator.numSubIdsForId(id)
       val numSubIds2 = generator.numSubIdsForId(id)
 
@@ -28,8 +28,8 @@ class GeneratorSpec extends AnyWordSpecLike with Matchers with ScalaFutures with
 
     "generate a reproducable list of subIds given an id" in {
       val generator = Generator(maxIndex = 100)
-      val id        = generator.indexToId(50)
-      val id2       = generator.indexToId(50)
+      val id        = generator.id(50)
+      val id2       = generator.id(50)
       val subIds    = generator.generateSubIds(id)
       val subIds2   = generator.generateSubIds(id2)
 
@@ -38,23 +38,23 @@ class GeneratorSpec extends AnyWordSpecLike with Matchers with ScalaFutures with
 
     "return which index created a specific id" in {
       val generator = Generator(maxIndex = 100)
-      val id        = generator.indexToId(50)
-      val idIndex   = generator.idToIndex(id)
+      val id        = generator.id(50)
+      val idIndex   = generator.index(id)
 
       idIndex shouldBe Some(50)
     }
 
     "return none if no index for a specific id was found" in {
       val generator = Generator(maxIndex = 100)
-      val id        = generator.indexToId(500)
-      val idIndex   = generator.idToIndex(id)
+      val id        = generator.id(500)
+      val idIndex   = generator.index(id)
 
       idIndex shouldBe None
     }
 
     "find if a subId is part of an id's sub group" in {
       val generator = Generator(maxIndex = 100)
-      val id        = generator.indexToId(50)
+      val id        = generator.id(50)
       val subIds    = generator.generateSubIds(id)
 
       subIds.foreach(subId => generator.isSubIdFromId(id, subId) shouldBe true)
@@ -62,7 +62,7 @@ class GeneratorSpec extends AnyWordSpecLike with Matchers with ScalaFutures with
 
     "find the id that generated a list of subIds" in {
       val generator = Generator(maxIndex = 100)
-      val id        = generator.indexToId(50)
+      val id        = generator.id(50)
       val subIds    = generator.generateSubIds(id)
 
       subIds.foreach(subId => generator.idFromSubId(subId) shouldBe Some(id))
@@ -71,10 +71,38 @@ class GeneratorSpec extends AnyWordSpecLike with Matchers with ScalaFutures with
     "find the index that generated a list of subIds" in {
       val generator = Generator(maxIndex = 100)
       val index     = 50
-      val id        = generator.indexToId(index)
+      val id        = generator.id(index)
       val subIds    = generator.generateSubIds(id)
 
       subIds.foreach(subId => generator.indexFromSubId(subId) shouldBe Some(50))
+    }
+
+    "sample a range of ids" in {
+      val generator = Generator(maxIndex = 100)
+      val ids       = generator.sample(10)
+
+      ids.length shouldBe 10
+      ids.distinct.lengthCompare(ids) shouldBe 0
+    }
+
+    "return two distinct ranges of ids" in {
+      val generator = Generator(maxIndex = 100)
+      val ids       = generator.sample(10)
+      val ids2      = generator.sample(10)
+
+      ids.length shouldBe 10
+      ids2.length shouldBe 10
+      ids shouldNot contain theSameElementsAs ids2
+    }
+
+    "loop round to the original ids if the max index is hit" in {
+      val generator = Generator(maxIndex = 10)
+      val ids       = generator.sample(10)
+      val ids2      = generator.sample(10)
+
+      ids.length shouldBe 10
+      ids2.length shouldBe 10
+      ids should contain theSameElementsAs ids2
     }
   }
 }
