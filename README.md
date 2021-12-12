d8f4470595f3c1601ce1df13970bfb0b07e2bbe6
Ben Carter <benjohn.carter@gmail.com>
2021-12-12 18:07:48 +0000

    refactor: make API easier to understand, add sample (#22)

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 [![Scala CI](https://github.com/bcarter97/id-generator/actions/workflows/scala.yml/badge.svg)](https://github.com/bcarter97/id-generator/actions/workflows/scala.yml)
 [![CI Release](https://github.com/bcarter97/id-generator/actions/workflows/release.yml/badge.svg)](https://github.com/bcarter97/id-generator/actions/workflows/release.yml)
-![Sonatype Nexus (Releases)](https://img.shields.io/nexus/r/io.github.bcarter97/id-generator_3?server=https%3A%2F%2Fs01.oss.sonatype.org)
-![Sonatype Nexus (Snapshots)](https://img.shields.io/nexus/s/io.github.bcarter97/id-generator_3?server=https%3A%2F%2Fs01.oss.sonatype.org)
+[![Sonatype Nexus (Releases)](https://img.shields.io/nexus/r/io.github.bcarter97/id-generator_3?server=https%3A%2F%2Fs01.oss.sonatype.org)](https://s01.oss.sonatype.org/content/repositories/releases/io/github/bcarter97/)
+[![Sonatype Nexus (Snapshots)](https://img.shields.io/nexus/s/io.github.bcarter97/id-generator_3?server=https%3A%2F%2Fs01.oss.sonatype.org)](https://s01.oss.sonatype.org/content/repositories/snapshots/io/github/bcarter97/)
 
 Generate reproducable UUIDs based of a sequence of numbers, with the possibility of generating sub UUIDs from the parent UUID.
 
@@ -22,7 +22,7 @@ import io.github.bcarter97.Generator
 
 object main extends App {
   val generator = Generator()
-  val id = generator.indexToId(5)
+  val id = generator.id(5)
   print(id)
   // e4da3b7f-bbce-3345-9777-2b0674a318d5
 }
--- src/main/scala/io/github/bcarter97/idgenerator/Generator.scala
+++ src/main/scala/io/github/bcarter97/idgenerator/Generator.scala
@@ -3,30 +3,37 @@ package io.github.bcarter97
 import java.util.UUID
 
 case class Generator(maxIndex: Int = 1000000, subIds: Int = 10) {
+  private var sampleCounter = 1;
 
-  def indexToId(index: Int, seed: Option[String] = None) =
+  def id(index: Int, seed: Option[String] = None) =
     UUID.nameUUIDFromBytes(s"${index.toString}${seed.getOrElse("")}".getBytes).toString
 
-  val idMap = (1 to maxIndex).map(index => (indexToId(index), index)).toMap
+  private val idMap = (1 to maxIndex).map(index => (id(index), index)).toMap
 
-  def idToIndex(id: String): Option[Int] = idMap.get(id)
+  def index(id: String): Option[Int] = idMap.get(id)
 
   def numSubIdsForId(id: String): Int =
     (BigInt(id.getBytes.slice(0, 1)) % subIds).toInt
 
-  def generateSubIds(id: String): Seq[String] = {
-    val subIds = numSubIdsForId(id)
-    (1 to subIds).map(i => indexToId(i, Some(id))).toList
+  def generateSubIds(parentId: String): Seq[String] = {
+    val subIds = numSubIdsForId(parentId)
+    (1 to subIds).map(i => id(i, Some(parentId))).toList
   }
 
-  def isSubIdFromId(id: String, subId: String): Boolean =
+  def isSubIdFromId(parentId: String, subId: String): Boolean =
     (1 to subIds).collectFirst {
-      case i if indexToId(i, Some(id)) == subId => i
+      case i if id(i, Some(parentId)) == subId => i
     }.isDefined
 
   def indexFromSubId(subId: String): Option[Int] =
-    (1 to maxIndex).collectFirst { case i if isSubIdFromId(indexToId(i), subId) => i }
+    (1 to maxIndex).collectFirst { case i if isSubIdFromId(id(i), subId) => i }
 
-  def idFromSubId(subId: String): Option[String] = indexFromSubId(subId).map(indexToId(_))
+  def idFromSubId(subId: String): Option[String] = indexFromSubId(subId).map(id(_))
+
+  def sample(n: Int = 1): Seq[String] = {
+    val ids = (sampleCounter to sampleCounter + n - 1).map(index => id(index % maxIndex))
+    sampleCounter += n
+    ids
+  }
 
 }
