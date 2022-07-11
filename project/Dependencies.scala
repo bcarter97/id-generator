c0fe9106973f4d5850409b5829df25fe3fcf3a2d
Scala Steward <scala_steward@virtuslab.com>
2022-07-11 13:09:49 +0000

    build(deps): update cats-core, cats-laws from 2.7.0 to 2.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.12" % Test
 
   object Cats {
-    private val cats = "2.7.0"
+    private val cats = "2.8.0"
     val core         = "org.typelevel" %% "cats-core" % cats
     val laws         = "org.typelevel" %% "cats-laws" % cats
     val all          = Seq(core, laws)
